fc82801abf410b0916b81f07339a4b4793862fd1
Andrew Chernigovsky <chernigovsky108@gmail.com>
2023-03-20 02:03:26 +0300

    validate

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 // import swiperMain from './components/swiper';
 import { tabs } from './components/tabs.js';
 import { accordion } from './components/accordion.js';
+import { validation  } from './components/validation.js';
 
 (($) => {
 	$(() => {
 		tabs()
 		accordion()
+		validation()
 	}
 )})(jQuery);
