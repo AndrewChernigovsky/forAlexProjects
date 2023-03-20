2e94fff24240689d28562237fb3c9d54360bf730
Andrew Chernigovsky <chernigovsky108@gmail.com>
2023-03-20 19:42:56 +0300

    validate

--- src/js/components/validation.js
+++ src/js/components/validation.js
@@ -1,8 +1,6 @@
-const formList = document.querySelectorAll(".js-form-block");
-
+const formList = document.querySelectorAll('.js-form-block');
 
 export function validation() {
-
 	if (!formList.length) return;
 
 	formList.forEach((form) => attachEvents(form));
@@ -14,46 +12,72 @@ export function validation() {
 		const email = parent.querySelector("input[data-name='email']");
 		const password = parent.querySelector("input[data-name='password']");
 		const passwordConfirm = parent.querySelector("input[data-name='passwordConfirm']");
+		const phone = parent.querySelector("input[data-name='phone']");
 
 		if (!formInputList.length) return;
 
 		const checkInputs = () => {
-			const usernameValue = username.value.trim();
-			const emailValue = email.value.trim();
-			const passwordValue = password.value.trim();
-			const passwordConfirmValue = passwordConfirm.value.trim();
+			let usernameValue;
+			let emailValue;
+			let passwordValue;
+			let passwordConfirmValue;
+			let phoneValue;
+			const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
 
-			if (!usernameValue) {
-				setErrorFor(username, 'Username cannot be blank');
-			} else {
-				setSuccessFor(username);
+			if (username !== null) {
+				usernameValue = username.value.trim();
+
+				if (!usernameValue) {
+					setErrorFor(username, 'Username cannot be blank');
+				} else {
+					setSuccessFor(username);
+				}
 			}
+			if (email !== null) {
+				emailValue = email.value.trim();
 
-			if (!emailValue) {
-				setErrorFor(email, 'Email cannot be blank');
-			} else if (!isEmail(emailValue)) {
-				setErrorFor(email, 'Email is not valid');
-			} else {
-				setSuccessFor(email);
+				if (!emailValue) {
+					setErrorFor(email, 'Email cannot be blank');
+				} else if (!isEmail(emailValue)) {
+					setErrorFor(email, 'Email is not valid');
+				} else {
+					setSuccessFor(email);
+				}
 			}
+			if (password !== null) {
+				passwordValue = password.value.trim();
 
-			const re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
-			if (!passwordValue) {
-				setErrorFor(password, 'Password cannot be blank');
-			} else if (passwordValue.length < 8) {
-				setErrorFor(password, 'Password to short');
-			} else if (!passwordValue.match(re)) {
-				setErrorFor(password, 'it have to contains a upper, lower and a number');
-			} else {
-				setSuccessFor(password);
+				if (!passwordValue) {
+					setErrorFor(password, 'Password cannot be blank');
+				} else if (passwordValue.length < 8) {
+					setErrorFor(password, 'Password to short');
+				} else if (!passwordValue.match(passwordRegex)) {
+					setErrorFor(password, 'it have to contains a upper, lower and a number');
+				} else {
+					setSuccessFor(password);
+				}
 			}
+			if (passwordConfirm !== null) {
+				passwordConfirmValue = passwordConfirm.value.trim();
 
-			if (!passwordConfirmValue) {
-				setErrorFor(passwordConfirm, 'write again your password');
-			} else if (passwordValue !== passwordConfirmValue) {
-				setErrorFor(passwordConfirm, 'does not match');
-			} else {
-				setSuccessFor(passwordConfirm);
+				if (!passwordConfirmValue) {
+					setErrorFor(passwordConfirm, 'write again your password');
+				} else if (passwordValue !== passwordConfirmValue) {
+					setErrorFor(passwordConfirm, 'does not match');
+				} else {
+					setSuccessFor(passwordConfirm);
+				}
+			}
+			if (phone !== null) {
+				phoneValue = phone.value.trim();
+
+				if (!phoneValue) {
+					setErrorFor(phone, 'phone cannot be blank');
+				} else if (!isPhone(phoneValue)) {
+					setErrorFor(phone, 'phone is not valid');
+				} else {
+					setSuccessFor(phone);
+				}
 			}
 		};
 
@@ -81,5 +105,8 @@ export function validation() {
 		return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
 			email
 		);
+	};
+	const isPhone = (phone) => {
+		return /^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/.test(phone);
 	};
 }
